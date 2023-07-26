35856beba4f1ca1fcb56469a940561e2177a2fb7
MahadBulle <mahadhussein012@gmail.com>
2023-07-26 08:41:45 -0700

    ct6

--- routes/PagesRoutes/contactRout.js
+++ routes/PagesRoutes/contactRout.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-// const contactController = require('../../Controllers/PagesControllers/contactController')
+const contactController = require('../../Controllers/PagesControllers/contactController')
 router.get('/',(req,res) => {
     res.send("waa uu shaqeenaa hada")
 })
 
-// router.get('/:id', contactController.GetContactById)
+router.get('/:id', contactController.GetContactById)
 
-// router.post('/', contactController.PostContact)
+router.post('/', contactController.PostContact)
 
-// router.put('/:id', contactController.PutContact)
+router.put('/:id', contactController.PutContact)
 
 module.exports = router
