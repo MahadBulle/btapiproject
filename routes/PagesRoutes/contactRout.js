d648da6094a4609e30d93c21305aec22e618c980
MahadBulle <mahadhussein012@gmail.com>
2023-07-26 08:49:10 -0700

    ct07

--- routes/PagesRoutes/contactRout.js
+++ routes/PagesRoutes/contactRout.js
@@ -7,10 +7,10 @@ router.get('/',(req,res) => {
     res.send("waa uu shaqeenaa hada")
 })
 
-router.get('/:id', contactController.GetContactById)
+// router.get('/:id', contactController.GetContactById)
 
-router.post('/', contactController.PostContact)
+// router.post('/', contactController.PostContact)
 
-router.put('/:id', contactController.PutContact)
+// router.put('/:id', contactController.PutContact)
 
 module.exports = router
