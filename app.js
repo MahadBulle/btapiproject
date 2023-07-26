2c0096c3131b81485ec99cd54a4d27924a4ce406
MahadBulle <mahadhussein012@gmail.com>
2023-07-26 07:53:56 -0700

    ch1

--- app.js
+++ app.js
@@ -4,31 +4,34 @@ const cors = require('cors');
 // const { MongoMemoryServer } = require('mongodb-memory-server-core'
 require('dotenv').config();
 
-const housesRoutes = require('./routes/housesRoutes')
-const usersRoutes = require('./routes/usersRoutes')
-const himagesRoutes = require('./routes/himagesRoutes')
-const homeSettingRoutes = require('./routes/PagesRoutes/homeSettingRouter')
-const galleryRoutes = require('./routes/PagesRoutes/galleryRoute')
-const ourClientRoutes = require('./routes/PagesRoutes/ourClientRoute')
-const aboutRoutes = require('./routes/PagesRoutes/aboutRoute')
-const contactRoutes = require('./routes/PagesRoutes/contactRout')
-const servicesRoutes = require('./routes/PagesRoutes/ourServicesRoute')
+// const housesRoutes = require('./routes/housesRoutes')
+// const usersRoutes = require('./routes/usersRoutes')
+// const himagesRoutes = require('./routes/himagesRoutes')
+// const homeSettingRoutes = require('./routes/PagesRoutes/homeSettingRouter')
+// const galleryRoutes = require('./routes/PagesRoutes/galleryRoute')
+// const ourClientRoutes = require('./routes/PagesRoutes/ourClientRoute')
+// const aboutRoutes = require('./routes/PagesRoutes/aboutRoute')
+// const contactRoutes = require('./routes/PagesRoutes/contactRout')
+// const servicesRoutes = require('./routes/PagesRoutes/ourServicesRoute')
 
 
 const app = express()
 
 app.use(express.json())
-app.use(cors({origin:['http://localhost:5173/']}));
+// app.use(cors({origin:['http://localhost:5173/']}));
 
-app.use('/houses', housesRoutes)
-app.use('/users', usersRoutes)
-app.use('/himages', himagesRoutes)
-app.use('/homesetting', homeSettingRoutes)
-app.use('/gallery', galleryRoutes)
-app.use('/ourclient', ourClientRoutes)
-app.use('/about', aboutRoutes)
-app.use('/contact', contactRoutes)
-app.use('/services', servicesRoutes)
+const Port = process.env.PORT
+  app.listen(Port);
+  console.log("welocme",Port);
+// app.use('/houses', housesRoutes)
+// app.use('/users', usersRoutes)
+// app.use('/himages', himagesRoutes)
+// app.use('/homesetting', homeSettingRoutes)
+// app.use('/gallery', galleryRoutes)
+// app.use('/ourclient', ourClientRoutes)
+// app.use('/about', aboutRoutes)
+// app.use('/contact', contactRoutes)
+// app.use('/services', servicesRoutes)
 
 
 mongoose.set('strictQuery', false)
@@ -39,9 +42,7 @@ mongoose.connect('mongodb+srv://generator:HkAXEuNAexSC358T@cluster0.fonguby.mong
   app.get('/',(req, res) => {
     res.send("wuu shaqayna")
   })
-  const Port = process.env.PORT
-  app.listen(Port);
-  console.log("welocme",Port);
+  
 // const connectiondb = async () => {
 //   const createServer = await MongoMemoryServer.create()
 //   await mongoose.connect(createServer.getUri(), { dbName: 'Betahouse' })
