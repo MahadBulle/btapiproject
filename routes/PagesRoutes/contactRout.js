d333c76a3a27dc5f686709f20fe176e0abbeaa51
MahadBulle <mahadhussein012@gmail.com>
2023-07-27 00:59:47 -0700

    opened crud contact

--- routes/PagesRoutes/contactRout.js
+++ routes/PagesRoutes/contactRout.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const router = express.Router()
+// const { ContactModel } = require('../../Models/PagesModels/contactModel')
 
 // const contactController = require('../../Controllers/PagesControllers/contactController')
 // const contactController = require('../../Controllers/PagesControllers/contactController')
+const {GetContactById,PostContact,PutContact} = require('../../Controllers/PagesControllers/contactController')
 router.get('/',(req,res) => {
+
     res.send("waa uu shaqeenaa hada")
 })
 
-// router.get('/:id', contactController.GetContactById)
+router.get('/:id',GetContactById)
 
-// router.post('/', contactController.PostContact)
+router.post('/', PostContact)
 
-// router.put('/:id', contactController.PutContact)
+router.put('/:id',PutContact)
 
 module.exports = router
